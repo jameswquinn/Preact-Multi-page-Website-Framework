becc7ab3b86cc3262cbac941b5e7cee9e65611c6
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-20 18:12:02 +0100

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+import { h } from 'preact';
+import { Router } from 'preact-router';
+import { lazy, Suspense } from 'preact/compat';
+import { Header } from './components/Header.jsx';
+import { Loading } from './components/Loading.jsx';
+
+const Home = lazy(() => import('./pages/Home.jsx'));
+const About = lazy(() => import('./pages/About.jsx'));
+const Contact = lazy(() => import('./pages/Contact.jsx'));
+
+export function App() {
+  return (
+    <div id="app">
+      <Header />
+      <Suspense fallback={<Loading />}>
+        <Router>
+          <Home path="/" />
+          <About path="/about" />
+          <Contact path="/contact" />
+        </Router>
+      </Suspense>
+    </div>
+  );
+}
